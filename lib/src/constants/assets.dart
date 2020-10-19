d93596131a06173f6987f6fd654206cac3335b84
Madhur Maurya <madhurmaurya365@gmail.com>
2020-10-19 15:35:16 +0530

    Illustration Assets

--- lib/src/constants/assets.dart
+++ lib/src/constants/assets.dart
@@ -1,6 +1,22 @@
 
 
 class Assets{
-  static String get githubLogo => 'assets/icons/github.png';
-  static String get googleLogo => 'assets/icons/google.png';
+  static const path = 'assets';
+  static Logos logos = Logos(path);
+  static Illustrations illustrations = Illustrations(path);
+}
+
+class Logos{
+  final String path;
+  Logos(String path) : this.path = '$path/icons';
+
+  String get github => '$path/github.png';
+  String get google => '$path/google.png';
+}
+
+class Illustrations{
+  final String path;
+  Illustrations(String path) : this.path = '$path/illustrations';
+
+  String get forgotPassword => '$path/drawkit-grape-pack-illustration-7.svg';
 }
